6b6fd62884d8a48a05380402dd5df8fe225ac14e
Lanndon <roselan@mail.gvsu.edu>
2015-11-13 11:48:48 -0500

    sorry it took so damn long but i got it.

--- src/Test.java
+++ src/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String [] args){
         System.out.println("just testing intellij");
         System.out.println("Just Testing Intellij");
+        System.out.println("I Got intellij to Work");
     }
 }
