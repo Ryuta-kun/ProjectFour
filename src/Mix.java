8fde1b2d1a57f7ab91b440ddd8a3a50e2a1b8ab4
ryo-kun <hiranor@mail.gvsu.edu>
2015-11-15 14:54:24 -0500

    added some stuff

--- src/Mix.java
+++ src/Mix.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 /**
  * Created by Ryo-chan on 11/11/15.
  */
 public class Mix implements IMix {
+    private LinkList message;
 
+    public Mix(){
+        message = new LinkList();
+    }
+
+    @Override
+    public void setInitialMessage(String message) {
+
+    }
+
+    @Override
+    public String processCommand(String command) {
+        return null;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Enter in initial message to mix up");
+        Scanner sc = new Scanner(System.in);
+        String message = sc.nextLine();
+        System.out.println("\nMessage: \n");
+        System.out.println("Command: ");
+        sc = new Scanner(System.in);
+    }
 }
--- src/MixTest.java
+++ src/MixTest.java
@@ -1,5 +1,18 @@
 /**
  * Created by Lanndon on 11/15/2015.
  */
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class MixTest {
+
+    @Test
+    public void testProcessCommand() {
+        Mix message = new Mix();
+        message.setInitialMessage ("This is a secret message");
+        String userMessage = message.processCommand("b a 0");
+        assertEquals("aThis is a secret message", userMessage);
+    }
+
 }
