a356627d92cf9989cdd64ebfb1b675a6bd6a5324
Ryu-kun <hiranor@mail.gvsu.edu>
2015-12-01 14:22:46 -0500

    error proofing

--- src/Mix.java
+++ src/Mix.java
@@ -4,28 +4,28 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-/*****************************************************************
- Graphical representation of a six sided die with various controls
- over the appearance.  Current value is constrained between 1 and 6.
-
- @author Ryuta Hirano
- @author Lanndon Rose
- @version November 2015
- *****************************************************************/
+/*************************************************************************
+ * Graphical representation of a six sided die with various controls
+ * over the appearance.  Current value is constrained between 1 and 6.
+ *
+ * @author Ryuta Hirano
+ * @author Lanndon Rose
+ * @version November 2015
+ ************************************************************************/
 public class Mix implements IMix {
 
-    //variable for the user message
+    /**variable for the user message*/
     private LinkList<Character> message;
 
-    //variable for the commands
+    /**variable for the commands*/
     private String commandos;
 
-    //variable for the clipboard function
+    /**variable for the clipboard function*/
     protected String clipBoard;
 
-    /*****************************************************************
-     Constructor that instantiates the message list and command string
-     *****************************************************************/
+    /*********************************************************************
+     * Constructor that instantiates the message list and command string
+     ********************************************************************/
     public Mix(){
 
         //setting message to a linklist of type characters
@@ -35,10 +35,10 @@ public class Mix implements IMix {
         commandos = "";
     }
 
-    /*****************************************************************
-     Constructor that input user message into a LinkList
-     @param message the user inputted string
-     *****************************************************************/
+    /*********************************************************************
+     * Constructor that input user message into a LinkList
+     * @param message the user inputted string
+     ********************************************************************/
     @Override
     public void setInitialMessage(String message) {
         LinkList<Character> m = new LinkList<Character>();
@@ -52,18 +52,22 @@ public class Mix implements IMix {
         this.message = m;
     }
 
-    /*****************************************************************
-     Constructor that takes user inputs as commands for modifying the message
-     @param command user inputted string
-     *****************************************************************/
+    /*********************************************************************
+     * Constructor that takes user inputs as commands for
+     * modifying the message
+     * @param command user inputted string
+     * @return - returns a string with values associated with each
+     * commands.
+     ********************************************************************/
     @Override
     public String processCommand(String command){
         //checks whether "b" command is entered
         if (command.charAt(0) == 'b') {
             try {
-                String str = "";
+                String str = this.message.toString();
                 String undo = "";
                 LinkList<String> com = new LinkList<String>();
+
                 // puts command into linklist
                 for (String retval : command.split(" ")){
 
@@ -75,20 +79,28 @@ public class Mix implements IMix {
 
                 }
 
-                //takes information
+                //takes information from the linked list
                 int num = Integer.parseInt(com.readList(2).getData());
-                for (int i = 0; i < com.readList(1).getData().length(); i++){
-                    str = this.message.change(com.readList(1).getData().charAt(i), num++);
-                }
+                if (num >= 0) {
+                    //adds each character strings (if more than one) to
+                    //the list and addes them onto the String str variable
+                    for (int i = 0; i < com.readList(1).getData().length(); i++) {
+                        str = this.message.change(com.readList(1).getData().charAt(i), num++);
+                    }
 
-                if(com.readList(1).getData().length() > 1){
-                    int number = Integer.parseInt(com.readList(2).getData()) + (com.readList(1).getData().length() - 1);
-                    undo = "x " + Integer.parseInt(com.readList(2).getData()) + " " + number;
+                    //sets the undo command for the unmix file.
+                    if (com.readList(1).getData().length() > 1) {
+                        int number = Integer.parseInt(com.readList(2).getData()) + (com.readList(1).getData().length() - 1);
+                        undo = "x " + Integer.parseInt(com.readList(2).getData()) + " " + number;
+                    } else {
+                        undo = "r " + Integer.parseInt(com.readList(2).getData());
+                    }
+                    //sets each series of undo commands onto this method.
+                    setCommandos(undo);
                 }else{
-                    undo = "r " + Integer.parseInt(com.readList(2).getData());
+                    throw new IllegalArgumentException();
                 }
 
-                setCommandos(undo);
                 return str;
             }catch(Exception b){
                 throw b;
@@ -96,14 +108,19 @@ public class Mix implements IMix {
         } else if (command.charAt(0) == 'r') {
             try {
                 int numr = Integer.parseInt(command.substring(2));
-                String str2 = this.message.delete(numr);
-                char deleted = message.getRemoved().getData();
-                if (deleted == ' '){
-                    deleted = '_';
+                if (numr >= 0) {
+                    String str2 = this.message.delete(numr);
+                    char deleted = message.getRemoved().getData();
+                    if (deleted == ' ') {
+                        deleted = '_';
+                    }
+                    String undo = "b " + deleted + " " + numr;
+                    setCommandos(undo);
+                    return str2;
+                }else{
+                    throw new IllegalArgumentException();
                 }
-                String undo = "b " + deleted + " " + numr;
-                setCommandos(undo);
-                return str2;
+
             }catch(Exception b){
                 throw b;
             }
@@ -114,7 +131,7 @@ public class Mix implements IMix {
                 String[] values = command.split(" ");    //couldn't find another way to do it so
                 int start = Integer.parseInt(values[1]);  //should work for now. need another way to split
                 int end = Integer.parseInt(values[2]);    //these commands.
-                if (((this.message.count() - 1) >= end) && (start <= end)) {
+                if (((this.message.count() - 1) >= end) && (start <= end) && (start >= 0)) {
                     for (int i = start; i <= end; i++) {
                         str = this.message.delete(start);
                         clipBoard += this.message.getRemoved().getData().toString();
@@ -128,28 +145,31 @@ public class Mix implements IMix {
                     setCommandos(undo);
                     return str;
                 }else{
-                    System.err.println("Command Unknown");
                     throw new IllegalArgumentException();
                 }
             }catch(Exception b){
-                return this.message.toString();
+                throw b;
             }
         } else if (command.charAt(0) == 'p') {
             try {
                 String str = "";
                 int start = Integer.parseInt(command.substring(2));
-                if (!clipBoard.equals("")) {
-                    for (int i = 0; i < clipBoard.length(); i++) {
-                        str = this.message.change(clipBoard.charAt(i), start);
-                        start++;
-                    }
+                if (start >= 0) {
+                    if (!clipBoard.equals("")) {
+                        for (int i = 0; i < clipBoard.length(); i++) {
+                            str = this.message.change(clipBoard.charAt(i), start);
+                            start++;
+                        }
 
-                    int num = Integer.parseInt(command.substring(2)) + clipBoard.length() - 1;
-                    String undo = "x " + Integer.parseInt(command.substring(2)) + " " + num;
-                    setCommandos(undo);
-                    return str;
-                } else {
-                    return this.message.toString();
+                        int num = Integer.parseInt(command.substring(2)) + clipBoard.length() - 1;
+                        String undo = "x " + Integer.parseInt(command.substring(2)) + " " + num;
+                        setCommandos(undo);
+                        return str;
+                    } else {
+                        return this.message.toString();
+                    }
+                }else{
+                    throw new IllegalArgumentException();
                 }
             }catch(Exception b){
                 throw b;
@@ -160,7 +180,7 @@ public class Mix implements IMix {
                 String[] values = command.split(" ");    //couldn't find another way to do it so
                 int start = Integer.parseInt(values[1]);  //should work for now. need another way to split
                 int end = Integer.parseInt(values[2]);    //these commands.
-                if (start <= end) {
+                if ((start <= end) && (start >= 0)) {
                     String str = this.message.copy(start, end);
                     clipBoard = str;
                 }else{
@@ -188,12 +208,16 @@ public class Mix implements IMix {
                 String[] values = command.split(" ");    //couldn't find another way to do it so
                 int num1 = Integer.parseInt(values[1]);  //should work for now. need another way to split
                 int num2 = Integer.parseInt(values[2]);    //these commands.
-                String str = "";
-                str = this.message.swapChar(num1, num2);
+                if ((num1 >= 0) && (num2 >= 0)) {
+                    String str = "";
+                    str = this.message.swapChar(num1, num2);
 
-                undo = "w " + num1 + " "+  num2;
-                setCommandos(undo);
-                return str;
+                    undo = "w " + num1 + " " + num2;
+                    setCommandos(undo);
+                    return str;
+                }else{
+                    throw new IllegalArgumentException();
+                }
             }catch (Exception e) {
                 throw e;
             }
@@ -201,12 +225,20 @@ public class Mix implements IMix {
         return this.message.toString();
     }
 
+    /*********************************************************************
+     * Method that shows the current message when called.
+     ********************************************************************/
     public void showMessage(){
         this.message.displayCounter();
         System.out.println();
         this.message.display();
     }
 
+    /*********************************************************************
+     * Saves the lists of undo commands in a file.
+     * @param str
+     * @param filename
+     ********************************************************************/
     public void save (String str, String filename) {
         if (!filename.contains(".txt")){
             filename = filename + ".txt";
@@ -223,34 +255,36 @@ public class Mix implements IMix {
         out.close();
     }
 
-    /*****************************************************************
-     Gets user inputted message
-     @return message
-     *****************************************************************/
+    /*********************************************************************
+     * Gets user inputted message
+     * @return message
+     ********************************************************************/
     public LinkList<Character> getMessage() {
         return message;
     }
 
-    /*****************************************************************
-     gets user inputted commands
-     @return commandos user inputted commands
-     *****************************************************************/
+    /*********************************************************************
+     * gets user inputted commands
+     * @return commandos user inputted commands
+     ********************************************************************/
     public String getCommandos() {
         return commandos;
     }
 
-    /*****************************************************************
-     Adds new user inputted commands to the list of previously inputted commands
-     @return commandos string of previous commands along with the most recent command
-     *****************************************************************/
+    /*********************************************************************
+     * Adds new user inputted commands to the list of previously
+     * inputted commands
+     * @return commandos string of previous commands along with the
+     * most recent command
+     ********************************************************************/
     public void setCommandos(String commandos) {
         this.commandos += commandos + "\n";
     }
 
-    /*****************************************************************
-     Main methond that starts the console program
-     @param args arguments of some sort I think
-     *****************************************************************/
+    /*********************************************************************
+     * Main methond that starts the console program
+     * @param args arguments of some sort I think
+     ********************************************************************/
     public static void main(String[] args){
         Mix m = new Mix();
 
