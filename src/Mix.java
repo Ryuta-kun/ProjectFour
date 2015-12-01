7899f06220ddc5399d7ceac1c9c5d111720c9be4
Ryu-kun <hiranor@mail.gvsu.edu>
2015-12-01 14:47:28 -0500

    error proofing

--- src/Mix.java
+++ src/Mix.java
@@ -107,13 +107,25 @@ public class Mix implements IMix {
             }
         } else if (command.charAt(0) == 'r') {
             try {
+                //pointer position for removal
                 int numr = Integer.parseInt(command.substring(2));
+
+                //throws an illegal argument exception if the pointer is
+                //less than 0
                 if (numr >= 0) {
+
+                    //string value that calls the delete method in linklist
                     String str2 = this.message.delete(numr);
+
+                    //this is to record the deleted message for undo.
                     char deleted = message.getRemoved().getData();
+
+                    //sets the space as "_" if space is needed
                     if (deleted == ' ') {
                         deleted = '_';
                     }
+
+                    //sets the undo command that unmixes this.
                     String undo = "b " + deleted + " " + numr;
                     setCommandos(undo);
                     return str2;
@@ -128,19 +140,36 @@ public class Mix implements IMix {
             try {
                 String str = "";
                 clipBoard = "";
+
+                //splits each cut command values and saves it in each
+                //array available.
                 String[] values = command.split(" ");    //couldn't find another way to do it so
                 int start = Integer.parseInt(values[1]);  //should work for now. need another way to split
                 int end = Integer.parseInt(values[2]);    //these commands.
+
+                //if the end pointer is less than the count and is greater
+                //than or equals to the start, it will initiate with one
+                //condition: start pointer must be greater than 0
+                // (this is to prevent negative numbers.
                 if (((this.message.count() - 1) >= end) && (start <= end) && (start >= 0)) {
+
+                    //goes through the start and end pointer and removes
+                    //each message. It will also save the removed value by
+                    //calling the getRemoved() method in the linklist class
+                    //to the clipboard
                     for (int i = start; i <= end; i++) {
                         str = this.message.delete(start);
                         clipBoard += this.message.getRemoved().getData().toString();
                     }
 
-
+                    //sets the space value as "_" if returns true
                     if (clipBoard.contains(" ")) {
                         clipBoard = clipBoard.replace(' ', '_');
                     }
+
+                    //sets the undo command by using the clipboard.
+                    //here instead of paste, we used the "b" command to
+                    //unmix the cut command
                     String undo = "b " + clipBoard + " " + start;
                     setCommandos(undo);
                     return str;
@@ -153,14 +182,31 @@ public class Mix implements IMix {
         } else if (command.charAt(0) == 'p') {
             try {
                 String str = "";
+
+                //this is the start pointer for the paste command
                 int start = Integer.parseInt(command.substring(2));
+
+                //initiates the following commands if and only if the start
+                //pointer is greater than or equal to 0 to prevent
+                //negative user inputs.
                 if (start >= 0) {
+
+                    //initiates the following command if clipboard is not
+                    //empty or null.
                     if (!clipBoard.equals("")) {
+
+                        //this goes through the clipboard and adds each
+                        //string as a character using charAt() method.
                         for (int i = 0; i < clipBoard.length(); i++) {
                             str = this.message.change(clipBoard.charAt(i), start);
                             start++;
                         }
 
+                        //this is for the undo commands.
+                        //on this int value, we added the start pointer
+                        //and the num value together to get where the
+                        //pasted commands are located and removes them
+                        //using the cut command.
                         int num = Integer.parseInt(command.substring(2)) + clipBoard.length() - 1;
                         String undo = "x " + Integer.parseInt(command.substring(2)) + " " + num;
                         setCommandos(undo);
@@ -177,10 +223,19 @@ public class Mix implements IMix {
         } else if (command.charAt(0) == 'c') {
             try {
                 clipBoard = "";
+
+                //splits each cut command values and saves it in each
+                //array available.
                 String[] values = command.split(" ");    //couldn't find another way to do it so
                 int start = Integer.parseInt(values[1]);  //should work for now. need another way to split
                 int end = Integer.parseInt(values[2]);    //these commands.
+
+                //initiates the following command if and only if the
+                //start pointer is less than the end pointer and greater
+                //than 0.
                 if ((start <= end) && (start >= 0)) {
+                    //string that calls the copy() command on the linklist
+                    //class to copy each command from start to end.
                     String str = this.message.copy(start, end);
                     clipBoard = str;
                 }else{
@@ -191,10 +246,15 @@ public class Mix implements IMix {
                 throw b;
             }
         } else if (command.charAt(0) == 's') {
+
+            //throws an exception if the file name area is empty
             if (command.charAt(2) == ' ') {
                 throw new IllegalArgumentException();
             }else {
                 try {
+                    //calls the previus undo commands (sorry for the
+                    //naming) and saves it as a string str3, and then
+                    //calls the method save to save the commands.
                     String str3 = getCommandos();
                     save(str3, command.substring(2));
                     return str3;
@@ -205,13 +265,23 @@ public class Mix implements IMix {
         } else if(command.charAt(0) == 'w'){
             try{
                 String undo = "";
+
+                //splits each cut command values and saves it in each
+                //array available.
                 String[] values = command.split(" ");    //couldn't find another way to do it so
                 int num1 = Integer.parseInt(values[1]);  //should work for now. need another way to split
                 int num2 = Integer.parseInt(values[2]);    //these commands.
+
+                //initiates if and only if each num values are greater than
+                // 0 to prevent negative user inputs.
                 if ((num1 >= 0) && (num2 >= 0)) {
                     String str = "";
+
+                    //this str value calls the swapChar in linklist and
+                    //swaps the two values entered.
                     str = this.message.swapChar(num1, num2);
 
+                    //sets the undo command for swap
                     undo = "w " + num1 + " " + num2;
                     setCommandos(undo);
                     return str;
